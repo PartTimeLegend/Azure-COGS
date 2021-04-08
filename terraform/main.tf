b4dae6c20e5290321bf29fcedff4e5c167f2943e
Antony Bailey <hi@antonybailey.net>
2021-04-08 09:27:13 +0100

    Tidy up terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "rgcogs" {
 
 resource "azurerm_automation_account" "aaacogs" {
   name                = "aaacogs${local.az_region_abbrv}"
-  location            = var.az_region
+  location            = azurerm_resource_group.rgcogs.location
   resource_group_name = azurerm_resource_group.rgcogs.name
   sku_name            = "Basic"
   depends_on = [
@@ -21,29 +21,37 @@ resource "azurerm_automation_account" "aaacogs" {
   ]
 }
 
-data "local_file" "runner_file" {
-  filename = "Runner.ps1"
+data "local_file" "startstop_file" {
+  filename = "StartStop.ps1"
 }
 
 resource "azurerm_automation_runbook" "arcogs" {
   name                    = "arcogs${local.az_region_abbrv}"
-  location                = var.az_region
+  location                = azurerm_automation_account.aaacogs.location
   resource_group_name     = azurerm_resource_group.rgcogs.name
   automation_account_name = azurerm_automation_account.aaacogs.name
   log_verbose             = "true"
   log_progress            = "true"
   description             = "Starts and Stops Resources based on times"
   runbook_type            = "PowerShell"
-  content                 = data.local_file.runner_file.content
+  content                 = data.local_file.startstop_file.content
   depends_on = [
     azurerm_resource_group.rgcogs,
     azurerm_automation_account.aaacogs,
-    data.local_file.runner_file
+    data.local_file.startstop_file
   ]
   publish_content_link {
     uri = "https://www.microsoft.com" # Placeholder as Azure is dumb
   }
 }
 output "runbook_content" {
-  value = data.local_file.runner_file.content
+  value = data.local_file.startstop_file.content
+}
+
+output "automation_account_name" {
+  value = azurerm_automation_account.aaacogs.name
+}
+
+output "runbook_name" {
+  value = azurerm_automation_runbook.arcogs.name
 }
