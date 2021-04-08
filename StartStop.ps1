0f1a619b819329a19e9024ec1ab5bc9ddb2d4f97
Antony Bailey <hi@antonybailey.net>
2021-04-08 17:39:14 +0100

    Add various start and stop and scale options

--- StartStop.ps1
+++ StartStop.ps1
@@ -16,30 +16,154 @@
 #>
 param(  
   [Parameter(Mandatory=$true)][bool]$dryRun=$true,
-  [Parameter(Mandatory=$false)][string]$prefix,
+  [Parameter(Mandatory=$false)][string]$prefix
 )
 $startDaysTagKey = "${prefix}StartDays"
 $stopDaysTagKey = "${prefix}StopDays"
+$apimDaySku = "Basic"
+$apimNightSku = "Developer"
 $resources = Get-AzResource
 $currentDay = (Get-Date -Format dddd)
 function Get-AzVMStatus
 {
   param(  
     [Parameter(Mandatory=$true)][string]$resourceGroup,
-    [Parameter(Mandatory=$true)][string]$vmName,
+    [Parameter(Mandatory=$true)][string]$vmName
   )
   return (Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Status).Statuses[1].DisplayStatus
+}
+function StartResource
+{
+  param(  
+    [Parameter(Mandatory=$true)]$resource
+  )
+  Write-FormattedOutput "INFO: Starting or scaling up resouce ${resource.ResourceId}" -ForegroundColor "Green"
+  switch ($resource.ResourceType)
+  {
+    "Microsoft.Compute/virtualMachines" 
+    {
+      if (Get-AzVMStatus $resource.ResourceGroupName $resource.Name -ne "VM running")
+      {
+        Start-AzVM -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+      }
+    }   
+    "Microsoft.ApiManagement/service"
+    {
+      # Scale up not create
+      $apim = Get-AzApiManagement -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+      $apim.Sku = $apimDaySku
+      Set-AzApiManagement -InputObject $apim
+    }     
+  }
+}
+function StopResource
+{
+  param(  
+    [Parameter(Mandatory=$true)]$resource
+  )
+  Write-FormattedOutput "INFO: Stopping, deleting, or scaling down resouce ${resource.ResourceId}" -ForegroundColor "Green"
+  switch ($resource.ResourceType)
+  {
+    "Microsoft.Compute/virtualMachines" 
+    {
+      if (Get-AzVMStatus $resource.ResourceGroupName $resource.Name -eq "VM running")
+      {
+        Stop-AzVM -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+      }
+    }
+    "Microsoft.Web/sites"
+    {
+      Remove-AzWebApp -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Storage/storageContainers"
+    {
+      Remove-AzStorageContainer -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Storage/storageAccounts"
+    {
+      Remove-AzStorageAccount -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.SignalRService/SignalR"
+    {
+      Remove-AzSignalR -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Sql/servers/databases"
+    {
+      Remove-AzSqlDatabase -ResourceGroupName $resource.ResourceGroupName -ServerName $resource.ServerName -DatabaseName $resouce.DatabaseName
+    }
+    "Microsoft.Sql/servers"
+    {
+      Remove-AzSqlServer  -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Insights/components"
+    {
+      Remove-AzApplicationInsights -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Web/serverFarms"
+    {
+      Remove-AzAppServicePlan -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.DocumentDb/databaseAccounts"
+    {
+      Remove-AzCosmosDBAccount -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.Search/searchServices"
+    {
+      Remove-AzSearchService -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.ServiceBus/namespaces"
+    {
+      Remove-AzServiceBusNamespace -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+    }
+    "Microsoft.ApiManagement/service"
+    {
+      # Scale down not delete.
+      $apim = Get-AzApiManagement -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
+      $apim.Sku = $apimNightSku
+      Set-AzApiManagement -InputObject $apim
+    }
+  }
+}
+function Write-FormattedOutput
+{
+    [CmdletBinding()]
+    Param(
+         [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][Object] $Object,
+         [Parameter(Mandatory=$False)][ConsoleColor] $BackgroundColor,
+         [Parameter(Mandatory=$False)][ConsoleColor] $ForegroundColor
+    )    
+
+    # save the current color
+    $bc = $host.UI.RawUI.BackgroundColor
+    $fc = $host.UI.RawUI.ForegroundColor
+
+    # set the new color
+    if($BackgroundColor -ne $null)
+    { 
+       $host.UI.RawUI.BackgroundColor = $BackgroundColor
+    }
+
+    if($ForegroundColor -ne $null)
+    {
+        $host.UI.RawUI.ForegroundColor = $ForegroundColor
+    }
+
+    Write-FormattedOutput $Object
+  
+    # restore the original color
+    $host.UI.RawUI.BackgroundColor = $bc
+    $host.UI.RawUI.ForegroundColor = $fc
 }
 foreach ($resource in $resources)
 {
   $startDaysValue = $resource.Tags[$startDaysTagKey]
   $stopDaysValue = $resource.Tags[$stopDaysTagKey]
   # Check for commented out. # will not be shutdown or started. These are manual ONLY.
-  if ($startDaysValue -like '#' or $stopDaysValue -like '#') 
+  if ($startDaysValue -like '#' -or $stopDaysValue -like '#') 
   {  
-    Write-Output "INFO: The resource $resource.ResouceId is exempt from start / stop functionality." 
-    Write-Output "INFO: The Start Days were ${startDaysValue} and the Stop Days were ${stopDaysValue} a # indicates skip."
-    return 0  
+    Write-FormattedOutput "INFO: The resource $resource.ResouceId is exempt from start / stop functionality." -$ForegroundColor "Green"
+    Write-FormattedOutput "INFO: The Start Days were ${startDaysValue} and the Stop Days were ${stopDaysValue} a # indicates skip." -$ForegroundColor "Green"
+    return 0
   }
   # Is today a day we do this for this resource?
   elseif ($startDaysValue -like $currentDay)
@@ -52,38 +176,20 @@ foreach ($resource in $resources)
     $stopTimeOfDay = [DateTime]::ParseExact($stopTimeValue,"HH:mm",$null).TimeOfDay
     if ($currentUtcTimeofDay -ge $startTimeOfDay -and $currentUtcTimeofDay -lt $stopTimeOfDay)
     {
-      # Start these resources
-      switch ($resource.ResourceType)
-      {
-        "Microsoft.Compute/virtualMachines" 
-        {
-          if (Get-AzVMStatus $resource.ResourceGroupName $resource.Name -ne "VM running")
-          {
-            Start-AzVM -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
-          }
-        }        
-      }
+      # Start this resource
+      StartResource $resource
     }
     elseif ($currentUtcTimeofDay -lt $startTimeOfDay -and $currentUtcTimeofDay -ge $stopTimeOfDay)
     {
-      # Stop these resources
-      switch ($resource.ResourceType)
-      {
-        "Microsoft.Compute/virtualMachines" 
-        {
-          if (Get-AzVMStatus $resource.ResourceGroupName $resource.Name -eq "VM running")
-          {
-            Stop-AzVM -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name
-          }
-        }        
-      }
+      # Stop this resource
+      StopResource $resouce
     }
     else
     {
-      Write-Output "ERROR: Some made up date happened."
-      Write-Output "ERROR: Resource is ${resource.ResourceId}."
-      Write-Output "ERROR: Current UTC Time of Day ${currentUtcTimeofDay}, Start Time Of Day ${startTimeOfDay}, Stop Time Of Day ${stopTimeOfDay}."
-      Write-Output "ERROR: You might want to find this."
+      Write-FormattedOutput "ERROR: Some made up date happened." -$BackgroundColor "Red"
+      Write-FormattedOutput "ERROR: Resource is ${resource.ResourceId}." -$BackgroundColor "Red"
+      Write-FormattedOutput "ERROR: Current UTC Time of Day ${currentUtcTimeofDay}, Start Time Of Day ${startTimeOfDay}, Stop Time Of Day ${stopTimeOfDay}." -$ForegroundColor "Red"
+      Write-FormattedOutput "ERROR: You might want to find this." -$BackgroundColor "Red"
     }
   }
 }
